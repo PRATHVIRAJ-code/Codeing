d32891e8021b50affc17fa5595d16ac5601da0af
PRATHVIRAJ-code <56132953+PRATHVIRAJ-code@users.noreply.github.com>
2019-10-04 09:28:02 +0530

    Add files via upload

---
+++ DuplicateSring.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // List<String> myStringList = new List<string>();
+            string[] a = { "this","is","test","is","to","test" };
+            string[] b = a.Distinct().ToArray();
+            foreach (string s in b)
+                Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
